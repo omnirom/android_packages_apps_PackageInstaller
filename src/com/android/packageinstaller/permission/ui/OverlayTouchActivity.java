7f78e0fd0cae837ce5a610e05df8df9f6c9327c9
Andreas Blaesius <skate4life@gmx.de>
2018-01-16 14:00:54 +0100

    Revert "Backport overlay security fix"
    This reverts commit 904542b657e36b85c775426bbae399875966206c.
    
    Change-Id: If087ce81c34342ce95294789ca06fbc471509767

--- src/com/android/packageinstaller/permission/ui/OverlayTouchActivity.java
+++ src/com/android/packageinstaller/permission/ui/OverlayTouchActivity.java
@@ -15,14 +15,33 @@
  */
 package com.android.packageinstaller.permission.ui;
 
-import static android.view.WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS;
-
 import android.app.Activity;
-import android.os.Bundle;
+import android.app.AppOpsManager;
+import android.os.Binder;
+import android.os.IBinder;
 
 public class OverlayTouchActivity extends Activity {
-    protected void onCreate(Bundle savedInstanceState) {
-        getWindow().addPrivateFlags(PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-        super.onCreate(savedInstanceState);
+    private final IBinder mToken = new Binder();
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setOverlayAllowed(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setOverlayAllowed(true);
+    }
+
+    private void setOverlayAllowed(boolean allowed) {
+        AppOpsManager appOpsManager = getSystemService(AppOpsManager.class);
+        if (appOpsManager != null) {
+            appOpsManager.setUserRestriction(AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
+                    !allowed, mToken);
+            appOpsManager.setUserRestriction(AppOpsManager.OP_TOAST_WINDOW,
+                    !allowed, mToken);
+        }
     }
 }
